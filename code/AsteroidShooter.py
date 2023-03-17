b699a7f50c621bf612003d75cf21699496b5ae66
Abel Alarcón <abelalarconv@gmail.com>
2023-03-17 13:17:34 -0300

    Ship basic class, show background and ship model

--- code/AsteroidShooter.py
+++ code/AsteroidShooter.py
@@ -1,6 +1,14 @@
 import pygame,sys
 from random import randint,uniform
 
+# Ship class
+class Ship(pygame.sprite.Sprite):
+    def __init__(self,groups):
+        super().__init__(groups) # 1. we have to init the parent class
+        self.image = pygame.image.load('../graphics/ship.png').convert_alpha()# 2. We need a surface -> image
+        self.rect = self.image.get_rect(center = (WINDOW_WIDTH /2, WINDOW_HEIGHT / 2))
+
+# Inicialize all imported pygame modules
 pygame.init()
 
 # -- Window --
@@ -8,21 +16,33 @@ WINDOW_WIDTH,WINDOW_HEIGHT = 1280,720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 pygame.display.set_caption('Asteroid Shooter')
 
+# Background
+background_surf = pygame.image.load('../graphics/background.png').convert()
 
+# Sprite groups
+spaceship_group = pygame.sprite.Group()
+# Ship sprite creation
+ship = Ship(spaceship_group)
 
-
+# Clock, I'll use to set a limit framerate
 clock = pygame.time.Clock()
+
+# Game loop
 while True:
+    # Delta time
     dt = clock.tick(120) / 1000
+    
+    # Events loop
     for event in pygame.event.get():
+        # Close event
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-
-
-
     # -- Drawing --
-    display_surface.fill((0,0,0))
+    # Show the background
+    display_surface.blit(background_surf, (0,0))
+    # Show the ship
+    spaceship_group.draw(display_surface)
 
     pygame.display.update()
