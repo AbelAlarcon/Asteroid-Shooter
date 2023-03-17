aa3189b438a7f761c2eca5e0ffb0512e5fd878d2
Abel Alarcón <abelalarconv@gmail.com>
2023-03-17 15:12:05 -0300

    Laser shoot timer logic

--- code/AsteroidShooter.py
+++ code/AsteroidShooter.py
@@ -3,10 +3,36 @@ from random import randint,uniform
 
 # Ship class
 class Ship(pygame.sprite.Sprite):
+
     def __init__(self,groups):
         super().__init__(groups) # 1. we have to init the parent class
         self.image = pygame.image.load('../graphics/ship.png').convert_alpha()# 2. We need a surface -> image
         self.rect = self.image.get_rect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
+        # Timer
+        self.can_shoot = True
+        self.shoot_time = None
+    
+    def laser_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.shoot_time > 500:
+                self.can_shoot = True
+
+    def input_position(self):
+        pos = pygame.mouse.get_pos()
+        self.rect.center = pos
+
+    def laser_shoot(self):
+        if pygame.mouse.get_pressed()[0] and self.can_shoot:
+            print('shoot laser')
+            self.can_shoot = False
+            self.shoot_time = pygame.time.get_ticks()
+
+    def update(self):
+        self.laser_timer()
+        self.laser_shoot()
+        self.input_position()
 
 # Laser class
 class Laser(pygame.sprite.Sprite):
@@ -48,7 +74,9 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
+        
+    # -- Update --
+    spaceship_group.update()
     # -- Drawing --
     # Show the background
     display_surface.blit(background_surf, (0,0))
