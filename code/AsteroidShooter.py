65a58c70fc059ee136a29cc861f53678e0015496
Abel Alarcón <abelalarconv@gmail.com>
2023-03-17 13:57:41 -0300

    Basic laser class

--- code/AsteroidShooter.py
+++ code/AsteroidShooter.py
@@ -6,7 +6,15 @@ class Ship(pygame.sprite.Sprite):
     def __init__(self,groups):
         super().__init__(groups) # 1. we have to init the parent class
         self.image = pygame.image.load('../graphics/ship.png').convert_alpha()# 2. We need a surface -> image
-        self.rect = self.image.get_rect(center = (WINDOW_WIDTH /2, WINDOW_HEIGHT / 2))
+        self.rect = self.image.get_rect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
+# Laser class
+class Laser(pygame.sprite.Sprite):
+    def __init__(self,groups,pos):
+        super().__init__(groups)
+        self.image = pygame.image.load('../graphics/laser.png').convert_alpha()
+        self.rect = self.image.get_rect(midbottom = pos)
+
 
 # Inicialize all imported pygame modules
 pygame.init()
@@ -20,9 +28,11 @@ pygame.display.set_caption('Asteroid Shooter')
 background_surf = pygame.image.load('../graphics/background.png').convert()
 
 # Sprite groups
-spaceship_group = pygame.sprite.Group()
+spaceship_group = pygame.sprite.GroupSingle()
+laser_group = pygame.sprite.Group()
 # Ship sprite creation
 ship = Ship(spaceship_group)
+laser = Laser(laser_group,(300,300))
 
 # Clock, I'll use to set a limit framerate
 clock = pygame.time.Clock()
@@ -44,5 +54,7 @@ while True:
     display_surface.blit(background_surf, (0,0))
     # Show the ship
     spaceship_group.draw(display_surface)
+    # Show the laser
+    laser_group.draw(display_surface)
 
     pygame.display.update()
